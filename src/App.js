8789e37fd4ba6199207447eb690ae1e6ccb25cb8
soheekim <soheekim.dev@gmail.com>
2021-07-12 20:11:14 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import Potato from './Potato';
+import React from 'react';
 
-function App() {
+////아래 두 개는 결과가 똑같다.
+
+//첫 번째 방법
+function Fruit1(props) {
+  return <h2>I like {props.fav}, {props.second}, and {props.third}.</h2>
+}
+
+function App1() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Potato />
+      <Fruit1 fav="strawberry" second="korean melon" third="banana" />
     </div>
   );
 }
 
-export default App;
+//두 번째 방법
+function Fruit2({ fav, second, third }) {
+  return <h2>I like {fav}, {second}, and {third}.</h2>
+}
+
+function App2() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Fruit2 fav="strawberry" second="korean melon" third="banana" />
+    </div>
+  );
+}
+
+
+////컴포넌트 재활용하기
+function Fruit3({ fav }) {
+  return <h2>I like {fav}.</h2>
+}
+
+function App3() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Fruit3 fav="strawberry" />
+      <Fruit3 fav="korean melon" />
+      <Fruit3 fav="banana" />
+    </div>
+  );
+}
+
+
+export default App3;
