44e3ad20479cb0349afa86e0200c81c49a4479a3
soheekim <soheekim.dev@gmail.com>
2021-07-12 20:45:55 +0900

    #2.3 map Recap - 3 Fruit component에 key prop 추가해서 에러 없애기

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ function Fruit({ name, picture }) {
 
 const fruitILike = [
   {
+    id: 1,
     name: "strawberry",
     image: "https://cdn.imweb.me/upload/S201804055ac60211e0c1d/09110318634f5.jpeg"
   },
   {
+    id: 2,
     name: "korean melon",
     image: "https://image.hmall.com/static/2/0/60/24/2124600238_0.jpg?RS=600x600&AR=0"
   },
   {
+    id: 3,
     name: "banana",
     image: "https://health.chosun.com/site/data/img_dir/2021/01/27/2021012700739_0.jpg"
   },
@@ -26,7 +29,7 @@ function App() {
   return (
     <div>
       {fruitILike.map(fruit => (
-        <Fruit name={fruit.name} picture={fruit.image} />
+        <Fruit key={fruit.id} name={fruit.name} picture={fruit.image} />
       ))}
     </div>
   );
