9f4f2545fd606ea97104a0c68ed8e47e80bb2bbf
soheekim <soheekim.dev@gmail.com>
2021-07-13 16:27:49 +0900

    #4.1 Rendering the Movies - 5
    로딩이 완료되면 Movie의 title 출력하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
   state = {
@@ -18,8 +19,11 @@ class App extends React.Component {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? "Loading..." : movies.map(movie => {
+      console.log(movie);
+      return <Movie id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} posoter={movie.medium_cover_image} />
+    })}</div>
   }
 }
 
