582b0d09c893363bd61eeeb5c7963d26e2cc920e
soheekim <soheekim.dev@gmail.com>
2021-07-13 13:46:56 +0900

    #4.1 Rendering the Movies - 1
    API에서 원하는 data에 접근, console에서 확인할 수 있다.

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ class App extends React.Component {
   };
   getMovies = async () => {
     const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    console.log(movies.data.data.movies);
   }
   componentDidMount() {
     this.getMovies();
   }
-
   render() {
     const { isLoading } = this.state;
     return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
@@ -21,10 +21,3 @@ class App extends React.Component {
 
 
 export default App;
-
-// 리액트 실행 순서
-// 1. App render
-// 2. isLoading: true
-// 3. isLoading ? "Loading..." : "We are ready"
-// 4. Application이 Mount된 후(componentDidMount), getMovies 함수 호출
-// 5. getMovies는 axios.get을 사용. 하지만 axios.get은 완료되기까지 시간이 조금 필요하기 때문에 await를 넣었음
