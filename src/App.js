68c7c894404efd436747bc38766738ad58fc9624
soheekim <soheekim.dev@gmail.com>
2021-07-13 11:47:34 +0900

    #3.3 Planning the Movie Component - 2

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from 'react';
 
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   };
   componentDidMount() {
     setTimeout(() => {
       this.setState({ isLoading: false });
     }, 6000);
   }
-
-  // render() {
-  //   return <div>{this.state.isLoading ? "Loading..." : "We are ready"}</div>;
-  // }
-
-  //위와 아래는 결과가 같음
 
   render() {
     const { isLoading } = this.state;  //Get me isLoading from the state.
