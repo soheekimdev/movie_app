da1ddfa262e3e768a27e1caaacbbfd96698b533f
soheekim <soheekim.dev@gmail.com>
2021-07-14 16:42:07 +0900

    #6.4 Redirecting - 2

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-function Detail(props) {
-    console.log(props);
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if(location.state === undefined) {
+      history.push("/");  //go back home
+    }
+  }
+
+  render() {
     return (
-        <span>Hello</span>
+      <span>Hello</span>
     )
+  }
 }
 
 export default Detail;
