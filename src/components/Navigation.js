65a74aab6d4edcf1968fb9d44da8d9894d71d6a7
soheekim <soheekim.dev@gmail.com>
2021-07-14 15:37:06 +0900

    #6.3 Sharing Props Between Routes - 1

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,12 @@ function Navigation() {
   return (
     <div className="nav">
       <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <Link to={{
+        pathname: "/about",
+        state: {
+          fromNavigation: true  //브라우저 console 창에서 props 중 location > state > fromNavigation: true가 추가된 걸 확인할 수 있음.
+        }
+      }}>About</Link>
     </div>
   )
 }
